f33c7e46389a682cedffe9b0b1a27653159663b3
HakuryuuDom <abohn49@yahoo.com>
2018-08-23 19:58:40 -0400

    fix

--- setting_migration.js
+++ setting_migration.js
@@ -6,7 +6,7 @@ let DefaultSettings = {
 	"targetLang": "en"
 }
 
-module.exports = function MigrateSetting(from_ver, to_ver, settings) {
+module.exports = function MigrateSettings(from_ver, to_ver, settings) {
 	if(from_ver === undefined) {
 		return Object.assign(Object.assign({}, DefaultSettings), settings);
 	} else if(from_ver === null) {
